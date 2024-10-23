03c52ec5cb683740e9bcf5ac332c7579507d8f8c
Shaun Chong <mumk0313@gmail.com>
2024-10-23 18:44:22 +0800

    i love c

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdint.h>
+#define then {
+#define end }
+#define peace return
+#define out 0
+#define is ==
+#define isnt !=
+#define or ||
+#define and &&
+#define now time(NULL)
+#if (__STDC_VERSION__ >= 199901L && __STDC_VERSION__ < 202000L)
+#include <stdbool.h>
+#elif __STDC_VERSION__ < 199901L
+#define true 1
+#define false 0
+typedef int _Bool;
+#define bool _Bool
+#endif
+typedef unsigned char uchar;
+typedef unsigned char ubyte;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef char* string;
+typedef char byte;
+typedef char* bytes;
+typedef void* any;
+
+#if defined(__GNUC__) || defined(__GNUG__)
+#define var __auto_type
+#define let __auto_type
+#define local __auto_type
+#elif __STDC_VERSION__ > 201710L || defined(__cplusplus)
+#define var auto
+#define let auto
+#define local auto
+#endif
+
+
+int add(int a, int b) then
+    return a + b;
+end
+
+int main(int argc, char** argv) then
+    printf("hello world\n");
+
+    if (1 is 2) then
+        printf("yes");
+    end
+
+    printf("4 + 6 = %d", add(4, 6));
+
+    peace out;
+end
