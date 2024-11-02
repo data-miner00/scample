5c71f0bf032bf315f4e3fe7aeec7561eb51079e9
data-miner00 <data-miner00@users.noreply.github.com>
2024-11-02 23:42:13 +0800

    refactor: Move header definitions

--- main.c
+++ main.c
@@ -5,40 +5,7 @@
 #include <stdint.h>
 #include <shaun.h>
 #include <vector.h>
-#define then {
-#define end }
-#define peace return
-#define out 0
-#define is ==
-#define isnt !=
-#define or ||
-#define and &&
-#define now time(NULL)
-#if (__STDC_VERSION__ >= 199901L && __STDC_VERSION__ < 202000L)
-#include <stdbool.h>
-#elif __STDC_VERSION__ < 199901L
-#define true 1
-#define false 0
-#endif
-typedef unsigned char uchar;
-typedef unsigned char ubyte;
-typedef unsigned short ushort;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-typedef char* string;
-typedef char byte;
-typedef char* bytes;
-typedef void* any;
-
-#if defined(__GNUC__) || defined(__GNUG__)
-#define var __auto_type
-#define let __auto_type
-#define local __auto_type
-#elif __STDC_VERSION__ > 201710L || defined(__cplusplus)
-#define var auto
-#define let auto
-#define local auto
-#endif
+#include <useless.h>
 
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
